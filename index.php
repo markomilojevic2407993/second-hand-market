0c64d5bd1c617221ab53be47a13b24c2446c7940
markomilojevic2407993 <markomilojevic2407993@gmail.com>
2023-12-02 13:53:34 +0100

    session select form

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
+}
+
+?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,13 +18,24 @@
     <title>Document</title>
 </head>
 <body>
+    <?php
+    if (isset($_SESSION['user'])) {
+        $user = $_SESSION['user'];
+        echo $user['name'];
+    }
 
-
-    <a href="login.php">Login</a>
+?>
+        <form action="login.php" method="post">
+            <input type="text" name="name" required placeholder="your name">
+            <input type="text" name="pass" required placeholder="your pass">
+            <button type="submit">Login</button>
+        </form>
+        <hr>
+    
     <form action="controller/registrat.php" method="post">
         <input type="text" name="name" placeholder="unesi svoje ima " required> 
         <input type="text" name="pass" placeholder="unesi svoj password" required>
-        <button type="submit">Unesi podatke u bazu</button>
+        <button type="submit">new user</button>
     </form>
     <hr>
 
@@ -22,7 +44,7 @@
         <input type="text" name="name" placeholder="name " required> 
         <input type="text" name="description" placeholder="description" required>
         <input type="number" name="price" placeholder="price">
-        <input type="hidden" name="userId" value="1">
+        <input type="hidden" name="userId" value="<?php echo $user['id']; ?>">
         <button type="submit">new product</button>
     </form>
 </body>
